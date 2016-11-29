008eb8a309d6627828e89546e0dbf22c2e65b65b
Riber <314420972@qq.com>
2016-11-29 11:15:27 +0800

    Initial Commit

---
+++ StarDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StarDemo
+//
+//  Created by Riber on 16/11/29.
+//  Copyright © 2016年 Riber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StarDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  StarDemo
+//
+//  Created by Riber on 16/11/29.
+//  Copyright © 2016年 Riber. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
